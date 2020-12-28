ee9fac94d72feb3f7be5a65a337fb7e9bef3196a
Dave <davekarry1337@gmail.com>
2020-12-28 12:24:00 +0500

    Добавлены тесты в tests.py

--- shop/WebShopSite/users/tests.py
+++ shop/WebShopSite/users/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
-
+from .models import Tovar, Category
 # Create your tests here.
+class CategoryTestCase(TestCase):
+    def setUp(self):
+        Category.objects.create(
+            name="Мед"
+        )
+        Category.objects.create(
+            name="Растение"
+        )
+
+    def test_get_name(self):
+        honey = Category.objects.get(name="Мед")
+        plant = Category.objects.get(name="Растение")
+        self.assertEqual(honey.__str__(), 'Мед')
+        self.assertEqual(plant.__str__(), 'НЕ растение!')
