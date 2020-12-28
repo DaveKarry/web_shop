9386401e215cf88c849e9499f4d1a8bfa1486e0f
Dave <davekarry1337@gmail.com>
2020-12-28 11:51:50 +0500

    Приведен views.py к единому стандарту view-based представлений, изменены формы, в которых могут быть ошибки с заполнением форм

--- shop/WebShopSite/users/views.py
+++ shop/WebShopSite/users/views.py
@@ -3,96 +3,111 @@ from time import timezone
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 from django.shortcuts import redirect
+from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate
 from django.contrib.auth import login
 from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib import messages
 from django.views.generic import  DetailView
 from django.views.generic import  View
 from django.core.exceptions import ObjectDoesNotExist
+from django.http import HttpResponseRedirect
+from django.utils.decorators import method_decorator
 from .models import *
 from .forms import *
 import time
-from django.http import HttpResponse
-from django.template.loader import get_template
-from xhtml2pdf import pisa
+
 
 # Create your views here.
 
-def index(request):
-    return render(request, 'users/index.html')
+class Index(View):
+    def get(self,*args, **kwargs):
+        return render(self.request, 'users/index.html')
 
-def contact(request):
-    return render(request, 'users/contact.html')
+class Contact(View):
+    def get(self,*args, **kwargs):
+        return render(self.request, 'users/contact.html')
 
-def about(request):
-    return render(request, 'users/about.html')
+class About(View):
+    def get(self,*args, **kwargs):
+        return render(self.request, 'users/about.html')
 
-def custom_logout(request):
-    logout(request)
-    return render(request, 'users/index.html')
+class CustomLogout(View):
+    def get(self,*args, **kwargs):
+        logout(self.request)
+        return render(self.request, 'users/index.html')
 
-def delete(request, slug):
-    tovar = get_object_or_404(Tovar, slug=slug)
-    tovar.delete()
-    return redirect('catalog')
+class DeleteTovar(View):
+    def get(self, *args, **kwargs):
+        print(kwargs)
+        tovar = get_object_or_404(Tovar, slug=kwargs['slug'])
+        tovar.delete()
+        return redirect('catalog')
 
-def register(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+class Register(View):
+    def get(self, *args, **kwargs):
+        form = UserCreationForm()
+        context = {'form': form}
+        return render(self.request, 'registration/register.html', context)
+
+    def post(self,*args, **kwargs):
+        form = UserCreationForm(self.request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data['username']
             password = form.cleaned_data['password1']
             user = authenticate(username=username, password=password)
-            login(request,user)
+            login(self.request, user)
             time.sleep(1)
             return redirect('index')
-    else:
-        form = UserCreationForm()
-    context = {'form': form}
-    return render(request,'registration/register.html', context)
+        else:
+            messages.info(self.request, 'Ошибка регистрации! Проверьте поля!')
+            return HttpResponseRedirect('register')
 
-def catalog(request):
-    tovars = Tovar.objects.order_by('price')
-    return render(request, 'shop/catalog.html',{'tovars':tovars})
+class Catalog(View):
+    def get(self,*args,**kwargs):
+        tovars = Tovar.objects.order_by('price')
+        return render(self.request, 'shop/catalog.html', {'tovars': tovars})
 
-def create_tovar(request):
-    if request.method == "POST":
-        print("Зашли")
-        form =TovarForm(request.POST)
+class CreateTover(View):
+    @method_decorator(login_required)
+    def get(self,*args,**kwargs):
+        form = TovarForm()
+        context = {'form': form}
+        return render(self.request, 'shop/create_tovar.html', context)
+    def post(self, *args,**kwargs):
+        form = TovarForm(self.request.POST)
         if form.is_valid():
             form.save()
             return redirect('catalog')
-    else:
-        form = TovarForm()
-        context = {'form': form}
-        return render(request, 'shop/create_tovar.html', context)
-
-@login_required
-def add_to_curent_order(request, slug):
-    tovar = get_object_or_404(Tovar,slug=slug)
-    orderItem, created = OrderItem.objects.get_or_create(item=tovar, user=request.user, ordered=False)
-    order_qs = Order.objects.filter(user=request.user, is_ordered=False)
-    if order_qs.exists():
-        current_order = order_qs[0]
-        if current_order.tovars.filter(item__slug=tovar.slug).exists():
-            orderItem.quantity +=1
-            orderItem.save()
         else:
+            messages.info(self.request, 'Какая-то ошибка! Прверьте поля или попробуйте снова!')
+            return HttpResponseRedirect('create_tovar')
+
+class AddToCurrentOrder(View):
+    @method_decorator(login_required)
+    def get(self,*args,**kwargs):
+        tovar = get_object_or_404(Tovar, slug=kwargs['slug'])
+        orderItem, created = OrderItem.objects.get_or_create(item=tovar, user=self.request.user, ordered=False)
+        order_qs = Order.objects.filter(user=self.request.user, is_ordered=False)
+        if order_qs.exists():
+            current_order = order_qs[0]
+            if current_order.tovars.filter(item__slug=tovar.slug).exists():
+                orderItem.quantity += 1
+                orderItem.save()
+            else:
+                current_order.tovars.add(orderItem)
+        else:
+            ordered_date = timezone.now()
+            current_order = Order.objects.create(user=self.request.user, ordered_date=ordered_date)
             current_order.tovars.add(orderItem)
-    else:
-        ordered_date = timezone.now()
-        current_order =Order.objects.create(user=request.user, ordered_date=ordered_date)
-        current_order.tovars.add(orderItem)
-    return  redirect("tovar", slug=slug)
+        return redirect("tovar", slug=kwargs['slug'])
 
 class TovarDetailView(DetailView):
     model = Tovar
-    template_name = "tovar.html"
+    template_name = "shop/tovar.html"
 
 class OrderView(LoginRequiredMixin, View):
     def get(self, *args, **kwargs):
@@ -104,102 +119,110 @@ class OrderView(LoginRequiredMixin, View):
             messages.error(self.request, "У вас нет заказов!")
             return redirect("/")
 
-@login_required
-def remove_from_order(request, slug):
-    item = get_object_or_404(Tovar, slug=slug)
-    order_qs = Order.objects.filter(
-        user=request.user,
-        is_ordered=False
-    )
-    if order_qs.exists():
-        order = order_qs[0]
-        # check if the order item is in the order
-        if order.tovars.filter(item__slug=item.slug).exists():
-            order_item = OrderItem.objects.filter(
-                item=item,
-                user=request.user,
-                ordered=False
-            )[0]
-            order.tovars.remove(order_item)
-            order_item.delete()
-            return redirect("order")
-        else:
-            return redirect("tovar", slug=slug)
-    else:
-        return redirect("tovar", slug=slug)
-
-@login_required
-def remove_single_item_from_order(request, slug):
-    item = get_object_or_404(Tovar, slug=slug)
-    order_qs = Order.objects.filter(
-        user=request.user,
-        is_ordered=False
-    )
-    if order_qs.exists():
-        order = order_qs[0]
-        if order.tovars.filter(item__slug=item.slug).exists():
-            order_item = OrderItem.objects.filter(
-                item=item,
-                user=request.user,
-                ordered=False
-            )[0]
-            if order_item.quantity > 1:
-                order_item.quantity -= 1
-                order_item.save()
-            else:
+class RemoveFromOrder(View):
+    @method_decorator(login_required)
+    def get(self,*args, **kwargs):
+        item = get_object_or_404(Tovar, slug=kwargs['slug'])
+        order_qs = Order.objects.filter(
+            user=self.request.user,
+            is_ordered=False
+        )
+        if order_qs.exists():
+            order = order_qs[0]
+            # check if the order item is in the order
+            if order.tovars.filter(item__slug=item.slug).exists():
+                order_item = OrderItem.objects.filter(
+                    item=item,
+                    user=self.request.user,
+                    ordered=False
+                )[0]
                 order.tovars.remove(order_item)
-            return redirect("order")
+                order_item.delete()
+                return redirect("order")
+            else:
+                return redirect("tovar", slug=kwargs['slug'])
         else:
-            return redirect("tovar", slug=slug)
-    else:
-        return redirect("tovar", slug=slug)
+            return redirect("tovar", slug=kwargs['slug'])
 
-@login_required
-def add_single_item_to_order(request, slug):
-    tovar = get_object_or_404(Tovar, slug=slug)
-    print("tut")
-    order_qs=Order.objects.filter(
-        user=request.user,
-        is_ordered=False
-    )
-    if order_qs.exists:
-        order = order_qs[0]
-        if order.tovars.filter(item__slug=tovar.slug).exists():
-            order_item = OrderItem.objects.filter(
-                item=tovar,
-                user=request.user,
-                ordered=False
-            )[0]
-            order_item.quantity += 1
-            order_item.save()
-            return redirect("order")
+class RemoveSingleItemFromOrder(View):
+    @method_decorator(login_required)
+    def get(self, *args, **kwargs):
+        item = get_object_or_404(Tovar, slug=kwargs['slug'])
+        order_qs = Order.objects.filter(
+            user=self.request.user,
+            is_ordered=False
+        )
+        if order_qs.exists():
+            order = order_qs[0]
+            if order.tovars.filter(item__slug=item.slug).exists():
+                order_item = OrderItem.objects.filter(
+                    item=item,
+                    user=self.request.user,
+                    ordered=False
+                )[0]
+                if order_item.quantity > 1:
+                    order_item.quantity -= 1
+                    order_item.save()
+                else:
+                    order.tovars.remove(order_item)
+                return redirect("order")
+            else:
+                return redirect("tovar", slug=kwargs['slug'])
         else:
-            return redirect("tovar", slug=slug)
-    else:
-        return redirect("tovar", slug=slug)
+            return redirect("tovar", slug=kwargs['slug'])
 
-def checkout(request):
-    return render(request, 'users/checkout.html')
+class AddSingleItemToOrder(View):
+    @method_decorator(login_required)
+    def get(self, *args, **kwargs):
+        tovar = get_object_or_404(Tovar, slug=kwargs['slug'])
+        order_qs = Order.objects.filter(
+            user=self.request.user,
+            is_ordered=False
+        )
+        if order_qs.exists:
+            order = order_qs[0]
+            if order.tovars.filter(item__slug=tovar.slug).exists():
+                order_item = OrderItem.objects.filter(
+                    item=tovar,
+                    user=self.request.user,
+                    ordered=False
+                )[0]
+                order_item.quantity += 1
+                order_item.save()
+                return redirect("order")
+            else:
+                return redirect("tovar", slug=kwargs['slug'])
+        else:
+            return redirect("tovar", slug=kwargs['slug'])
 
-def profile(request):
-    addresses = Adress.objects.filter(
-        user = request.user
-    )
-    context = {'addresses': addresses}
-    return render(request, 'users/profile.html',context)
+class Checkout(View):
+    def get(self,*args,**kwargs):
+        return render(self.request, 'users/checkout.html')
 
-def add_address(request):
-    if request.method == "POST":
-        form = AdressForm(request.POST)
+class Profile(View):
+    def get(self, *args, **kwargs):
+        addresses = Adress.objects.filter(
+            user=self.request.user
+        )
+        context = {'addresses': addresses}
+        return render(self.request, 'users/profile.html', context)
+
+class AddAddress(View):
+    def get(self, *args, **kwargs):
+        form  = AdressForm()
+        context = {
+            'form': form
+        }
+        return render(self.request, 'users/add_address.html', context)
+    def post(self, *args,**kwargs):
+        form = AdressForm(self.request.POST or None)
         if form.is_valid():
             address = form.save(commit=False)
-            address.user = request.user
+            address.user = self.request.user
             form.save()
             return redirect('profile')
-    else:
-        form = AdressForm()
-        context = {'form': form}
-        return render(request, 'users/add_address.html', context)
+        else:
+            return redirect('add_address')
 
 class UpdateEmailView(View):
     def get(self, *args, **kwargs):
